5279aa86fc3227c6f1acb0956a5b4c6a55c389b9
yussufhh <0793477632g@gmail.com>
2024-07-04 23:26:38 +0300

    complete-react-hooks-information-flow-lab

---
+++ src/components/Filter.js
@@ -0,0 +1,15 @@
+
+function Filter({selectedCategory, onCategoryChange}){ 
+   return(  <div className="Filter">
+    <select name="filter" value={selectedCategory} onChange={onCategoryChange}>
+<option value="All">Filter by category</option>
+<option value="Produce">Produce</option>
+<option value="Dairy">Dairy</option>
+<option value="Dessert">Dessert</option>
+</select>
+    </div>)
+
+
+}
+
+export default Filter;
